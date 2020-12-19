6650a7cdd4b3f9f12bb20335a51d56c6d84398f7
Daniel Yagodaev <danielyag@gmail.com>
2020-12-19 11:46:57 +0200

    Update max sub product solution

--- src/problems/max_sub_product/Tester.java
+++ src/problems/max_sub_product/Tester.java
@@ -4,36 +4,36 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MaxProductTester {
+public class Tester {
 
     @Test
     public void test1(){
         int[] nums = {1,2,3};
-        assertEquals(MaxSubArrayProduct.maxSubArrayProductRec(nums), 6);
+        assertEquals(Solution.maxSubArrayProduct(nums), 6);
     }
 
     @Test
     public void test2(){
         int[] nums = {1,2,0,4,5};
-        assertEquals(MaxSubArrayProduct.maxSubArrayProductRec(nums), 20);
+        assertEquals(Solution.maxSubArrayProduct(nums), 20);
     }
 
     @Test
     public void test3(){
         int[] nums = {-4,5,9,-2,-8,12,6};
-        assertEquals(MaxSubArrayProduct.maxSubArrayProductRec(nums), 51840);
+        assertEquals(Solution.maxSubArrayProduct(nums), 51840);
     }
 
     @Test
     public void test4(){
         int[] nums = {-1,2,0,4,-2,8,-13,11,-5,0,12,4,-5,-6};
-        assertEquals(MaxSubArrayProduct.maxSubArrayProductRec(nums), 9152);
+        assertEquals(Solution.maxSubArrayProduct(nums), 9152);
     }
 
     @Test
     public void test5(){
         int[] nums = {-9,-2,-8,-7,-1,-3,-6,-5,-4};
-        assertEquals(MaxSubArrayProduct.maxSubArrayProductRec(nums), 90720);
+        assertEquals(Solution.maxSubArrayProduct(nums), 90720);
     }
 
 }
