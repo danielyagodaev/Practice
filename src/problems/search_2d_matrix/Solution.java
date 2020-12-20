07dd4cdcb39ee789355c6e98b084d7cb36286011
Daniel Yagodaev <danielyag@gmail.com>
2020-12-20 20:19:13 +0200

    Add search in 2d matrix problem

---
+++ src/problems/search_2d_matrix/Solution.java
@@ -0,0 +1,39 @@
+package problems.search_2d_matrix;
+
+public class Solution {
+
+    /**
+     * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+     * Integers in each row are sorted from left to right.
+     * The first integer of each row is greater than the last integer of the previous row.
+     * Constraints:
+     * m == matrix.length
+     * n == matrix[i].length
+     * 0 <= m, n <= 100
+     * -10^4 <= matrix[i][j], target <= 10^4
+     */
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int start = 0, end = rows * columns - 1;
+        while (start <= end){
+            int mid = (int) ((start + end) / 2);
+            int i = (int) (mid / columns);
+            int j = (int) (mid % columns);
+            int val = matrix[i][j];
+            if (val == target){
+                return true;
+            }
+            else if (val > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/problems/search_2d_matrix/Tester.java
@@ -0,0 +1,29 @@
+package problems.search_2d_matrix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tester {
+
+    @Test
+    public void test1(){
+        int[][] arr = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+        int target = 3;
+        assertEquals(true, Solution.searchMatrix(arr, target));
+    }
+
+    @Test
+    public void test2(){
+        int[][] arr = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+        int target = 13;
+        assertEquals(false, Solution.searchMatrix(arr, target));
+    }
+
+    @Test
+    public void test3(){
+        int[][] arr = {};
+        int target = 0;
+        assertEquals(false, Solution.searchMatrix(arr, target));
+    }
+}
