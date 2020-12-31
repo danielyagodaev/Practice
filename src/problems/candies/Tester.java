1492a5b9e8dc860c515b29989645ebbdf23d0c54
danielyag <daniel.yagodaev@kaltura.com>
2020-12-31 16:19:11 +0200

    Add candies problem

---
+++ src/problems/candies/Tester.java
@@ -0,0 +1,20 @@
+package problems.candies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tester {
+
+    @Test
+    public void test1(){
+        int[] ratings = {1,0,2};
+        assertEquals(Solution.candy(ratings), 5);
+    }
+
+    @Test
+    public void test2(){
+        int[] ratings = {1,2,2};
+        assertEquals(Solution.candy(ratings), 4);
+    }
+}
