33f4594ec0176906ab774972487e832064dc5b7c
Daniel Yagodaev <danielyag@gmail.com>
2021-01-02 10:39:30 +0200

    Add reverse words in a string problem

---
+++ src/problems/general/reverse_words_in_string/Readme.md
@@ -0,0 +1,14 @@
+Given an input string _s_, reverse the order of the **words**.
+
+A **word** is defined as a sequence of non-space characters. The **words** in _s_ will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+**Note** that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should
+only have a single space separating the words. Do not include any extra spaces.
+
+**Constraints:**
+
+- 1 <= _s.length_ <= 10^4
+- _s_ contains English letters (upper-case and lower-case), digits, and spaces ' '.
+- There is at least one word in _s_.
---
+++ src/problems/general/reverse_words_in_string/Solution.java
@@ -0,0 +1,28 @@
+package problems.general.reverse_words_in_string;
+
+public class Solution {
+
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i=s.length()-1, j;
+        while (i >= 0){
+            while (i>=0 && s.charAt(i) == ' '){
+                i--;
+            }
+            if (i < 0){
+                break;
+            }
+            j=i;
+            while (i>=0 && s.charAt(i) != ' '){
+                i--;
+            }
+            for (int k=i+1; k<=j; k++){
+                sb.append(s.charAt(k));
+            }
+            sb.append(' ');
+        }
+        int n = sb.length();
+        return sb.toString().substring(0, n-1);
+    }
+
+}
